e54ca7881161e55ed9509af13e00d17d8311c630
R2D2-like <tsukurobokun@gamil.com>
2023-06-16 19:34:09 +0900

    work through ros

--- hsr_pred.py
+++ hsr_pred.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import open3d as o3d
 import numpy as np
 import matplotlib.pyplot as plt
--- hsr_train.py
+++ hsr_train.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import json
 from pathlib import Path
 from typing import List, Optional
