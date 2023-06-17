298722a8cdaed30671d29cfa07c4e31840c77734
R2D2-like <tsukurobokun@gamil.com>
2023-06-17 14:17:31 +0900

    worked

--- http_pred_trigger.py
+++ http_pred_trigger.py
@@ -8,21 +8,32 @@ from hsr_collection.srv import CFPred, CFPredResponse
 
 
 class PredTrigger:
+    def __init__(self):
+        rospy.init_node('http_pred_trigger')
+
+        self.pred_srv = rospy.Service('/cf_pred', CFPred, self.pred)
+
+        rospy.loginfo("http_pred_trigger is initialized")
+
+    def pred(self, req):
+        self.query = req.query
+        return CFPredResponse(self.client())
+
     def client(self):
-        query = "snack"
         conn = http.client.HTTPConnection("localhost", 8000)
-        conn.request("POST", "/cf_pred", json.dumps({"query": query}))
+        conn.request("POST", "/cf_pred", json.dumps({"query": self.query}))
         response = conn.getresponse().read()
         # python dictに変換
         response = json.loads(response)
         print(response)
-        coordinates = response[0][query]
+        coordinates = response[0][self.query]
         print(coordinates)
+        return coordinates
 
 if __name__ == '__main__':
     # rospy.init_node('http_train_trigger')
     trigger = PredTrigger()
-    trigger.client()
+    rospy.spin()
     # while not rospy.is_shutdown():
     #     trigger.client()
     #     time.sleep(1)
--- http_train_trigger.py
+++ http_train_trigger.py
@@ -4,8 +4,29 @@ import json
 import time
 import rospy
 from std_msgs.msg import String
+from std_srvs.srv import Empty, EmptyResponse
 
 class TrainTrigger:
+    def __init__(self):
+        rospy.init_node('http_train_trigger')
+        self.start_trigger = rospy.Service('/cf_train/start', Empty, self.start)
+        self.stop_trigger = rospy.ServiceProxy('/cf_train/stop', Empty)
+
+        self.start_flag = False
+
+        rospy.loginfo("http_train_trigger is initialized")
+
+        while not rospy.is_shutdown():
+            if self.start_flag:
+                self.client()
+                self.start_flag = False
+                self.stop_trigger()
+            time.sleep(1)
+
+    def start(self, req):
+        self.start_flag = True
+        return EmptyResponse()
+
     def client(self):
         conn = http.client.HTTPConnection("localhost", 8080)
         conn.request("POST", "/cf_train", json.dumps({"name": "test"}))
@@ -15,7 +36,7 @@ class TrainTrigger:
 if __name__ == '__main__':
     # rospy.init_node('http_train_trigger')
     trigger = TrainTrigger()
-    trigger.client()
+    # trigger.client()
     # while not rospy.is_shutdown():
     #     trigger.client()
     #     time.sleep(1)
